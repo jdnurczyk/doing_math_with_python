272da1bab4c4b7acf50b7c720953837263d44ab0
Josh Nurczyk <jdnurczyk@gmail.com>
2019-06-01 15:40:24 -0400

    Initial multiplication table

--- mult_tables.py
+++ mult_tables.py
@@ -5,5 +5,10 @@ The user will specify the highest number to multiply by.
 '''
 
 def print_table(x, y):
-    for i in range(1, y+1):
-        
+    for i in range(1, int(y)+1):
+        print('{:>2}  x {:>2} = {:>3}'.format(x,i,(x*i)))
+
+if __name__ == '__main__':
+    num = int(input("Enter the number to generate a multiplication table: "))
+    last = int(input("Enter the highest number to multiply the number by: "))
+    print_table(num, last)

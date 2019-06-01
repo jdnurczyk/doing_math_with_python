b829f5d2a1bd8059a78323803b07f82721413f16
jdnurczyk <jdnurczyk@gmail.com>
2019-06-01 14:49:56 -0400

    Initial commit

---
+++ even_odd.py
@@ -0,0 +1,28 @@
+'''
+Even/Odd Vending Machine
+This will:
+1. Print whether the number is even/odd.
+2. Display that number with the next 9 similar (i.e. even or odd) numbers.
+3. Display an error message if a float is input.
+
+'''
+
+def is_even(n):
+    if n % 2 == 0:
+        print('Even')
+    else:
+        print('Odd')
+
+def print_nums(n):
+    for i in range(n,(n+20),2):
+        print(i)
+
+if __name__ == '__main__':
+    end_test = True
+    num = 0
+    while end_test:
+        num = input('Even/Odd Vending Machine! Enter an integer: ')
+        end_test = not float(num).is_integer()
+
+    is_even(int(num))
+    print_nums(int(num))
---
+++ mult_tables.py
@@ -0,0 +1,9 @@
+'''
+Enhanced Multiplication Table Generator
+Lets the user print a multiplication table, starting at 1, for any number.
+The user will specify the highest number to multiply by.
+'''
+
+def print_table(x, y):
+    for i in range(1, y+1):
+        

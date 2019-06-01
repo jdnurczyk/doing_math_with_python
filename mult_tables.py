6becd73806227d0486702971a2fb3a26181247da
Josh Nurczyk <jdnurczyk@gmail.com>
2019-06-01 16:11:56 -0400

    Changes added for error detection

--- mult_tables.py
+++ mult_tables.py
@@ -6,9 +6,17 @@ The user will specify the highest number to multiply by.
 
 def print_table(x, y):
     for i in range(1, int(y)+1):
-        print('{:>2}  x {:>2} = {:>3}'.format(x,i,(x*i)))
+        print('{:>2} x {:>2} = {:>3}'.format(x,i,(x*i)))
 
 if __name__ == '__main__':
-    num = int(input("Enter the number to generate a multiplication table: "))
-    last = int(input("Enter the highest number to multiply the number by: "))
+    print("Floating point numbers will be converted to integers.")
+    print("Using very large numbers will break the formatting of the table.")
+    try:
+        num = int(input("Enter an integer to generate a multiplication table: "))
+    except:
+        exit("Input was not an integer. Please try again.")
+    try:
+        last = int(input("Enter the highest integer to multiply the number by: "))
+    except:
+        exit("Input was not an integer. Please try again.")
     print_table(num, last)
